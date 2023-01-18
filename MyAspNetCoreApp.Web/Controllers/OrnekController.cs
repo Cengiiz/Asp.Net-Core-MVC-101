c041f9ed61579e4b82802f0eaf51d758072a419b
Cengiiz <dadem64@gmail.com>
2023-01-18 15:07:42 +0300

    Passing Data(Viewbag-ViewData-TempData-ViewModel)

--- MyAspNetCoreApp.Web/Controllers/OrnekController.cs
+++ MyAspNetCoreApp.Web/Controllers/OrnekController.cs
@@ -2,15 +2,36 @@
 
 namespace MyAspNetCoreApp.Web.Controllers
 {
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
     public class OrnekController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            ViewBag.name = "Asp.Net Core ";
+            ViewData["age"] = 30;
+            ViewData["names"] = new List<string>() { "ahmet", "mehmet", "hasan" };
+
+            ViewBag.person = new { id = 1, name = "ahmet", age = 23 };
+
+            TempData["animals"] = new List<string>() { "dog", "cat", "bird" };
+
+
+            var productList = new List<Product>()
+            {
+                new(){ Id=1, Name="Kalem"},
+                new(){ Id = 2, Name = "Defter"},
+                new(){ Id=3, Name="Silgi"}
+            };
+
+            return View(productList);
         }
         public IActionResult Index2()
         {
-            return RedirectToAction("Index");
+            return View();
         }
         public IActionResult ParametreView(int id)
         {
--- MyAspNetCoreApp.Web/Program.cs
+++ MyAspNetCoreApp.Web/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller}/{action}/{id?}");
+    pattern: "{controller=ornek}/{action=index}/{id?}");
 
 app.Run();
