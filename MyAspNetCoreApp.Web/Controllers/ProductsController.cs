9bb9f92b98cf92acaed2d6ab32cd6bc867225df6
Cengiiz <dadem64@gmail.com>
2023-01-22 16:23:54 +0300

    Adding new columns to the Product table

--- MyAspNetCoreApp.Web/Controllers/ProductsController.cs
+++ MyAspNetCoreApp.Web/Controllers/ProductsController.cs
@@ -16,9 +16,9 @@ namespace MyAspNetCoreApp.Web.Controllers
             //Linq method
             if(!_context.Products.Any())
             {
-                _context.Products.Add(new Product { Name = "Kalem 1", Price = 100, Stock = 300 });
-                _context.Products.Add(new Product { Name = "Kalem 2", Price = 200, Stock = 400 });
-                _context.Products.Add(new Product { Name = "Kalem 3", Price = 300, Stock = 500 });
+                _context.Products.Add(new Product { Name = "Kalem 1", Price = 100, Stock = 300, Color = "Red", Height = 10, Width = 20 });
+                _context.Products.Add(new Product { Name = "Kalem 2", Price = 200, Stock = 400, Color = "Red", Height = 10, Width = 20 });
+                _context.Products.Add(new Product { Name = "Kalem 3", Price = 300, Stock = 500, Color = "Red", Height = 10, Width = 20 });
                 _context.SaveChanges();
             }
             
