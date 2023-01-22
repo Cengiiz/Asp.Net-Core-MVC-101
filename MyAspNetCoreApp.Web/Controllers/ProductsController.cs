91c7dae97ad07250f34b63c1ccb48d2910de1be4
Cengiiz <dadem64@gmail.com>
2023-01-22 16:07:55 +0300

    Saving data in the product table

--- MyAspNetCoreApp.Web/Controllers/ProductsController.cs
+++ MyAspNetCoreApp.Web/Controllers/ProductsController.cs
@@ -5,17 +5,28 @@ namespace MyAspNetCoreApp.Web.Controllers
 {
     public class ProductsController : Controller
     {
+        private AppDbContext _context;
         private readonly ProductRepository _productRepository;
 
-        public ProductsController()
+        public ProductsController(AppDbContext context)//dependency injection pattern
         {
+            //DI Container
             _productRepository = new ProductRepository();
-
+            _context = context;
+            //Linq method
+            if(!_context.Products.Any())
+            {
+                _context.Products.Add(new Product { Name = "Kalem 1", Price = 100, Stock = 300 });
+                _context.Products.Add(new Product { Name = "Kalem 2", Price = 200, Stock = 400 });
+                _context.Products.Add(new Product { Name = "Kalem 3", Price = 300, Stock = 500 });
+                _context.SaveChanges();
+            }
+            
         }
 
         public IActionResult Index()
         {
-            var products = _productRepository.GetAll();
+            var products = _context.Products.ToList();
             return View(products);
         }
 
