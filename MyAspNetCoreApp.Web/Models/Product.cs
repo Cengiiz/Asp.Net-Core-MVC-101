91cfa69bc908f81e35c2eb545ccf8179c46e4ddf
Cengiiz <dadem64@gmail.com>
2023-01-23 15:04:59 +0300

    .Net Nullable Property

--- MyAspNetCoreApp.Web/Models/Product.cs
+++ MyAspNetCoreApp.Web/Models/Product.cs
@@ -3,7 +3,8 @@
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        //.net 6 ile nullable ozelligi proptan ayarlanabilir 
+        public string? Name { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
 
