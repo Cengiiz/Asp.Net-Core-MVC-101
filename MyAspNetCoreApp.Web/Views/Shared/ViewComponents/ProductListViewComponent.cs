02937b4d90a7a9e5ae1caefcc08e7912bb1b8de7
Cengiiz <dadem64@gmail.com>
2023-01-27 18:17:10 +0300

    View Component 3

--- MyAspNetCoreApp.Web/Views/Shared/ViewComponents/ProductListViewComponent.cs
+++ MyAspNetCoreApp.Web/Views/Shared/ViewComponents/ProductListViewComponent.cs
@@ -1,6 +1,28 @@
-﻿namespace MyAspNetCoreApp.Web.Views.Shared.ViewComponents
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAspNetCoreApp.Web.Models;
+using MyAspNetCoreApp.Web.ViewModels;
+
+namespace MyAspNetCoreApp.Web.Views.Shared.ViewComponents
 {
-    public class ProductListViewComponent
+    public class ProductListViewComponent:ViewComponent
     {
+        private readonly AppDbContext _context;
+
+        public ProductListViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var viewmodles = _context.Products.Select(x => new ProductListComponentViewModel()
+            {
+                Name=x.Name,
+                Description=x.Description
+            }).ToList();
+
+            return View(viewmodles);
+        }
+
     }
 }
