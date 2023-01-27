eb971f94d48be8e70bb63ae62dabc1f3014e7ed0
Cengiiz <dadem64@gmail.com>
2023-01-27 17:43:50 +0300

    Partial View Model Binding 2

--- MyAspNetCoreApp.Web/ViewModels/ProductListPartialViewModel.cs
+++ MyAspNetCoreApp.Web/ViewModels/ProductListPartialViewModel.cs
@@ -2,12 +2,12 @@
 {
     public class ProductListPartialViewModel
     {
-        public List<ProductListPartialViewModel> Products { get; set; }
-    }
+        public List<ProductPartialViewModel> Products { get; set; }
+    } 
     public class ProductPartialViewModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
     }
