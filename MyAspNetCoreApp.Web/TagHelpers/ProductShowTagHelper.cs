d33eee3bb88c780bd96bf9d56d8d5cfd50c19ef8
Cengiiz <dadem64@gmail.com>
2023-01-28 22:40:06 +0300

    Tag Helper Example 2 :<product-show>

---
+++ MyAspNetCoreApp.Web/TagHelpers/ProductShowTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using MyAspNetCoreApp.Web.Models;
+
+namespace MyAspNetCoreApp.Web.TagHelpers
+{
+    public class ProductShowTagHelper:TagHelper
+    {
+        public Product product { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Content.SetHtmlContent(@$"<ul class='list-group'>
+                        <li class='list-group-item'>{product.Id}</li>
+                        <li class='list-group-item'>{product.Name}</li>
+                        <li class='list-group-item'>{product.Price}</li>
+                        <li class='list-group-item'>{product.Stock}</li>
+                        </ul>");
+        }
+    }
+}
