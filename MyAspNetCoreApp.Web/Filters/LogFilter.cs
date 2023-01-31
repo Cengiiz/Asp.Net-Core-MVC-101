594542e7a511d85ef2606a6f22a18761d3ade62a
Cengiiz <dadem64@gmail.com>
2023-01-31 14:13:07 +0300

    ActionFilterAttribute

---
+++ MyAspNetCoreApp.Web/Filters/LogFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace MyAspNetCoreApp.Web.Filters
+{
+    public class LogFilter:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            Debug.WriteLine("Action Method calismadan once");
+        }
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            Debug.WriteLine("Action Method calistiktan sonra");
+
+        }
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            Debug.WriteLine("Action Method sonuc uretilmeden once");
+
+        }
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            Debug.WriteLine("Action Method sonuc uretildikten sonra");
+        }
+    }
+}
