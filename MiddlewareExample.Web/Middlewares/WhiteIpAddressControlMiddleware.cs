efa9749c33642fb7a6891975ee80439535a79e70
Cengiiz <dadem64@gmail.com>
2023-02-01 16:36:27 +0300

    Middleware Example -1

---
+++ MiddlewareExample.Web/Middlewares/WhiteIpAddressControlMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace MiddlewareExample.Web.Middlewares
+{
+    public class WhiteIpAddressControlMiddleware
+    {
+        private readonly RequestDelegate _requestDalegate;
+
+        public WhiteIpAddressControlMiddleware(RequestDelegate requestDalegate)
+        {
+            _requestDalegate = requestDalegate;
+        }
+
+        public async Task InvokeAsync(HttpContent context)
+        {
+            //IPV4 => 127.0.0.1  Localhost
+            //IPV6 => ::1 => Localhost
+
+
+
+
+        }
+    }
+}
